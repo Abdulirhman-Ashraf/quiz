dea08ff9c68a205112a1cf3094abfcc812aa0234
Abdulirhman-Ashraf <153239328+Abdulirhman-Ashraf@users.noreply.github.com>
2024-04-17 01:40:13 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,6 +10,16 @@ let btn9 = document.querySelector("#btn9");
 let quiz = document.querySelector(".quiz");
 let container = document.querySelector(".container");
 let quizOne = document.querySelector(".quizOne");
+let quiz1 = document.getElementsByName("q1");
+let quiz2 = document.getElementsByName("q2");
+let quiz3 = document.getElementsByName("q3");
+let quiz4 = document.getElementsByName("q4");
+let quiz5 = document.getElementsByName("q5");
+let quiz6 = document.getElementsByName("q6");
+let quiz7 = document.getElementsByName("q7");
+let quiz8 = document.getElementsByName("q8");
+let quiz9 = document.getElementsByName("q9");
+let quiz10 = document.getElementsByName("q10");
 let labelOne = document.querySelector(".labelOne");
 let quizTwo = document.querySelector(".quizTwo");
 let quizThree = document.querySelector(".quizThree");
@@ -24,49 +34,121 @@ let output1 = document.querySelector(".output");
 let inputs = document.querySelector("input");
 let quizNum = document.querySelector("#quizNum");
 let lis = document.querySelectorAll("ul li");
+let div1= document.createElement("div")
+
+for (let i = 0; i < quiz1.length; i++) {
+  quiz1[i].addEventListener("click", function () {
+   div1.style.cssText="width:100%; height:100%; position: absolute; right: 30px;top: 150px;"
+    document.body.append(div1)
+  });
+}
+for (let i = 0; i < quiz1.length; i++) {
+  quiz2[i].addEventListener("click", function () {
+   div1.style.cssText="width:100%; height:100%; position: absolute; right: 30px;top: 150px;"
+    document.body.append(div1)
+  });
+}
+for (let i = 0; i < quiz1.length; i++) {
+  quiz3[i].addEventListener("click", function () {
+   div1.style.cssText="width:100%; height:100%; position: absolute; right: 30px;top: 150px;"
+    document.body.append(div1)
+  });
+}
+for (let i = 0; i < quiz1.length; i++) {
+  quiz4[i].addEventListener("click", function () {
+   div1.style.cssText="width:100%; height:100%; position: absolute; right: 30px;top: 150px;"
+    document.body.append(div1)
+  });
+}
+for (let i = 0; i < quiz1.length; i++) {
+  quiz5[i].addEventListener("click", function () {
+   div1.style.cssText="width:100%; height:100%; position: absolute; right: 30px;top: 150px;"
+    document.body.append(div1)
+  });
+}
+for (let i = 0; i < quiz1.length; i++) {
+  quiz6[i].addEventListener("click", function () {
+   div1.style.cssText="width:100%; height:100%; position: absolute; right: 30px;top: 150px;"
+    document.body.append(div1)
+  });
+}
+for (let i = 0; i < quiz1.length; i++) {
+  quiz7[i].addEventListener("click", function () {
+   div1.style.cssText="width:100%; height:100%; position: absolute; right: 30px;top: 150px;"
+    document.body.append(div1)
+  });
+}
+for (let i = 0; i < quiz1.length; i++) {
+  quiz8[i].addEventListener("click", function () {
+   div1.style.cssText="width:100%; height:100%; position: absolute; right: 30px;top: 150px;"
+    document.body.append(div1)
+  });
+}
+for (let i = 0; i < quiz1.length; i++) {
+  quiz9[i].addEventListener("click", function () {
+   div1.style.cssText="width:100%; height:100%; position: absolute; right: 30px;top: 150px;"
+    document.body.append(div1)
+  });
+}
+for (let i = 0; i < quiz1.length; i++) {
+  quiz10[i].addEventListener("click", function () {
+   div1.style.cssText="width:100%; height:100%; position: absolute; right: 30px;top: 150px;"
+    document.body.append(div1)
+  });
+}
 btn1.addEventListener("click", function () {
+  div1.style.display = "none";
   quizTwo.style.display = "block";
   quizOne.style.display = "none";
   lis[1].classList.add("active");
-
 });
-
 btn2.addEventListener("click", function () {
+  div1.style.display = "none";
   quizTwo.style.display = "none";
   quizThree.style.display = "block";
   lis[2].classList.add("active");
 });
 btn3.addEventListener("click", function () {
+  div1.style.display = "none";
+
   quizThree.style.display = "none";
   quizFour.style.display = "block";
   lis[3].classList.add("active");
 });
 btn4.addEventListener("click", function () {
+  div1.style.display = "none";
+
   quizFour.style.display = "none";
   quizFive.style.display = "block";
   lis[4].classList.add("active");
 });
-btn5.addEventListener("click", function () {
+btn5.addEventListener("click", function () {  div1.style.display = "none";
+
+
   quizFive.style.display = "none";
   quizSix.style.display = "block";
   lis[5].classList.add("active");
 });
-btn6.addEventListener("click", function () {
+btn6.addEventListener("click", function () {  div1.style.display = "none";
+
   quizSix.style.display = "none";
   quizSeven.style.display = "block";
   lis[6].classList.add("active");
 });
-btn7.addEventListener("click", function () {
+btn7.addEventListener("click", function () {  div1.style.display = "none";
+
   quizSeven.style.display = "none";
   quizEight.style.display = "block";
   lis[7].classList.add("active");
 });
-btn8.addEventListener("click", function () {
+btn8.addEventListener("click", function () {  div1.style.display = "none";
+
   quizEight.style.display = "none";
   quizNine.style.display = "block";
   lis[8].classList.add("active");
 });
-btn9.addEventListener("click", function () {
+btn9.addEventListener("click", function () {  div1.style.display = "none";
+
   quizNine.style.display = "none";
   quizTen.style.display = "block";
   lis[9].classList.add("active");
